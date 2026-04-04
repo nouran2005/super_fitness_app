6bda68a04fec23466079ce7a3114883d0b64f9dd
MohammedGhazaly <mohammedelghazaly67@gmail.com>
2026-04-04 11:40:32 +0200

    - Added json serlizable annotation for the signin response

--- lib/features/signin/data/models/response/signin_response.dart
+++ lib/features/signin/data/models/response/signin_response.dart
@@ -1,6 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
 import 'package:super_fitness_app/features/signin/domain/entities/signin_entity.dart';
 
+part 'signin_response.g.dart';
+
+@JsonSerializable()
 class SigninResponse {
+  const SigninResponse();
+
+  factory SigninResponse.fromJson(Map<String, dynamic> json) =>
+      _$SigninResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SigninResponseToJson(this);
+
   SigninEntity toSigninEntity() {
     return SigninEntity();
   }
