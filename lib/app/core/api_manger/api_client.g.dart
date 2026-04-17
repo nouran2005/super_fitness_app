e475011a15448107a04708b4f6fa16af4fc19086
Nouran Samer <nouransamer5@gmail.com>
2026-04-17 22:40:57 +0200

    feat(SCRUM-5): update dependencies and generated mock files for Mockito

--- lib/app/core/api_manger/api_client.g.dart
+++ lib/app/core/api_manger/api_client.g.dart
@@ -6,7 +6,7 @@ part of 'api_client.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations,unused_element_parameter
 
 class _ApiClient implements ApiClient {
   _ApiClient(this._dio, {this.baseUrl, this.errorLogger}) {
