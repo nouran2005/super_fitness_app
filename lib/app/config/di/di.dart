9da682f9f8dc14309229495493715dcc06560ae7
alibesar7 <alibesar28@gmail.com>
2026-04-26 16:25:54 +0300

    feat(SCRUM-15): SCRUM-15-ex

--- lib/app/config/di/di.dart
+++ lib/app/config/di/di.dart
@@ -1,6 +1,5 @@
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:injectable/injectable.dart'; 
 import 'package:super_fitness_app/app/config/di/di.config.dart';
 
 final getIt = GetIt.instance;
